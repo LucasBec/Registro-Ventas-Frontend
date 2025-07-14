ba0eb33c1a74a5f773e1ae1f626d4a3a302d8ccd
LucasBec <89425792+LucasBec@users.noreply.github.com>
2025-07-13 23:43:07 -0300

    fc

--- src/app/services/ventas.service.ts
+++ src/app/services/ventas.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
-import { environment } from '../../environments/environment.prod';
+import { environment } from '../../environments/environment';
 
 export interface Venta {
   id?: number;
